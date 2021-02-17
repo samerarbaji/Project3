466ce41bfbb6f391d1b4e837237eef6aeab028de
samerarbaji <70446042+samerarbaji@users.noreply.github.com>
2021-02-16 22:13:32 -0500

    Update app.js

--- SportsbettingApp/static/js/app.js
+++ SportsbettingApp/static/js/app.js
@@ -1,17 +1,20 @@
-d3.json("/api/HorseRoute").then(function(data) {
-  console.log(data)
+
+
+
+  d3.json("/api/UFCRoute"), function (data) {
+    console.log(data)
   
     
   
     
-    });
+    };
   
 
 //function buildPlot() {
   /* data route */
   const url = "/api/HorseRoute";
-  d3.json(url).then(function(response) {
+  d3.json(url), function(response) {
 
     console.log(response);
-  });
+  };
 
