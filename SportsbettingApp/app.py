12a300e449ed3b67e82ece7d2ff1b0153326f82b
glennmacapinlac <71099457+glennmacapinlac@users.noreply.github.com>
2021-02-16 23:21:02 -0500

    Update app.py

--- SportsbettingApp/app.py
+++ SportsbettingApp/app.py
@@ -1,14 +1,18 @@
 # import necessary libraries
 import os
+import pandas as pd
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import (create_engine, func)
 from flask import (
     Flask,
     render_template,
+    url_for,
+    json,
     jsonify,
     request,
-    redirect,
-    url_for)
-from flask_sqlalchemy import SQLAlchemy
-
+    redirect)
 
 
 
@@ -22,11 +26,12 @@ app = Flask(__name__)
 # Database Setup
 #################################################
 
-
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import func
 app.config['SQLALCHEMY_DATABASE_URI'] = "postgres:samer123@localhost:5432/project2_db"
 
 # Remove tracking modifications
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+#app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
@@ -37,22 +42,15 @@ def home():
     return render_template("index.html")
 
 
-class NFLRoute(db.Model):
-    __tablename__ = 'NFL betting'
 
-    Home_Win = db.Column(db.String(64), primary_key=True)
-    Home_Team = db.Column(db.String(64))
-    Away_Team = db.Column(db.String(64))
-    Away_Odds = db.Column(db.String(64))
-    Home_Odds = db.Column(db.String(64))
     
-    def __repr__(self):
-        return '<tStats %r>' % (self.name)
+   
 
 @app.route("/api/NFLRoute")
 #returns data of all odds from a requested team jsonified
-def NFLRoute2():
-    results1 = db.session.query(
+def NFLRoute():
+    
+    results1 = session.query(
     NFLRoute.Home_Win, 
     NFLRoute.Home_Team, 
     NFLRoute.Away_Team, 
@@ -61,13 +59,13 @@ def NFLRoute2():
     
     result_list1 = []
     
-    for result in results1:
+    for result1 in results1:
         NFLdata = {
-            "Home Team":item[1],
-            "Away Team":item[2],
-            "Home Win":item[3],
-            "Home Odds":item[4],
-            "Away Odds":item[5]
+            "Home Team": result1[1],
+            "Away Team": result1[2],
+            "Home Win": result1[3],
+            "Home Odds": result1[4],
+            "Away Odds": result1[5]
         }
 
         result_list1.append(NFLdata)
@@ -75,65 +73,46 @@ def NFLRoute2():
     return jsonify(result_list1)
 
 
-class HorseRoute(db.Model):
-    __tablename__ = 'Horse betting'
 
-    Result = db.Column(db.String(64), primary_key=True)
-    Horse = db.Column(db.String(64))
-    BetType = db.Column(db.String(64))
-    Track= db.Column(db.String(64))
-    Odds = db.Column(db.String(64))
     
-    def __repr__(self):
-        return '<tStats %r>' % (self.name)
+    
 
 @app.route("/api/HorseRoute")
 #returns horse betting jsonified data
-def NFLRoute():
-    results1 = db.session.query(
+def HorseRoute():
+    
+    results2 = session.query(
     HorseRoute.Result, 
     HorseRoute.Horse, 
     HorseRoute.BetType, 
     HorseRoute.Track, 
     HorseRoute.Odds).all()
     
-    result_list1 = []
+    result_list2 = []
     
-    for result in results1:
+    for result2 in results2:
         Horsedata = {
-            "Track":item[1],
-            "Horse":item[2],
-            "Bet Type":item[3],
-            "Odds":item[4],
-            "Result":item[5]
+            "Track": result2[1],
+            "Horse": result2[2],
+            "Bet Type": result2[3],
+            "Odds": result2[4],
+            "Result": result2[5]
         }
 
-        result_list1.append(Horsedata)
+        result_list2.append(Horsedata)
 
-    return jsonify(result_list1)
+    return jsonify(result_list2)
 
-class UFCRoute(db.Model):
-    __tablename__ = 'UFC betting'
 
-    Winner1 = db.Column(db.String(64), primary_key=True)
-    Winner2 = db.Column(db.String(64), primary_key=True)
-    Red_Corner_Fighter1 = db.Column(db.String(64))
-    Blue_Corner_Fighter1 = db.Column(db.String(64))
-    Red_Fighter_odds1 = db.Column(db.String(64))
-    Blue_Fighter_odds1 = db.Column(db.String(64))
-    Red_Corner_Fighter2 = db.Column(db.String(64))
-    Blue_Corner_Fighter2 = db.Column(db.String(64))
-    Red_Fighter_odds2 = db.Column(db.String(64))
-    Blue_Fighter_odds2 = db.Column(db.String(64))
     
-    def __repr__(self):
-        return '<tStats %r>' % (self.name)
+    
 
 @app.route("/api/UFCRoute")
 #returns UFC fight data jsonified
-def UFCRoute2():
-    results1 = db.session.query(
-    NFLRoute.Winner1, 
+def UFCRoute():
+   
+    results3 = session.query(
+    UFCRoute.Winner1, 
     UFCRoute.Winner2, 
     UFCRoute.Red_Corner_Fighter1, 
     UFCRoute.Blue_Corner_Fighter1, 
@@ -144,25 +123,25 @@ def UFCRoute2():
     UFCRoute.Red_Fighter_odds2,
     UFCRoute.Blue_Fighter_odds2).all()
     
-    result_list1 = []
+    result_list3 = []
     
-    for result in results1:
+    for result3 in results3:
         UFCdata = {
-            "Red Corner Fighter 1":item[1],
-            "Blue Corner Fighter 1":item[2],
-            "Red Fighter 1 Odds":item[3],
-            "Blue Fighter 1 Odds":item[4],
-            "Winner of Fight 1":item[5],
-            "Red Corner Fighter 2":item[6],
-            "Blue Corner Fighter 2":item[7],
-            "Red Fighter 2 Odds":item[8],
-            "Blue Fighter 2 Odds":item[9],
-            "Winner of Fight 2":item[10]
+            "Red Corner Fighter 1": result3[1],
+            "Blue Corner Fighter 1": result3[2],
+            "Red Fighter 1 Odds": result3[3],
+            "Blue Fighter 1 Odds": result3[4],
+            "Winner of Fight 1": result3[5],
+            "Red Corner Fighter 2": result3[6],
+            "Blue Corner Fighter 2": result3[7],
+            "Red Fighter 2 Odds": result3[8],
+            "Blue Fighter 2 Odds": result3[9],
+            "Winner of Fight 2": result3[10]
         }
 
-        result_list1.append(UFCdata)
+        result_list3.append(UFCdata)
 
-    return jsonify(result_list1)
+    return jsonify(result_list3)
 
 if __name__ == "__main__":
     app.run(debug=True)
